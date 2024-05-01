3c917600a804eeeff5dc6841d5875c6c51a68100
Thanh Dinh <thanhdinhbao@outlook.com.vn>
2024-05-01 19:35:48 +0700

    Add project files.

---
+++ CheckLiveCCV/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace CheckLiveCCV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtUnknown = new System.Windows.Forms.TextBox();
+            this.txtLive = new System.Windows.Forms.TextBox();
+            this.txtDie = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            this.txtRaw = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(189, 335);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(101, 59);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtUnknown
+            // 
+            this.txtUnknown.Location = new System.Drawing.Point(26, 62);
+            this.txtUnknown.Multiline = true;
+            this.txtUnknown.Name = "txtUnknown";
+            this.txtUnknown.Size = new System.Drawing.Size(239, 155);
+            this.txtUnknown.TabIndex = 1;
+            // 
+            // txtLive
+            // 
+            this.txtLive.Location = new System.Drawing.Point(288, 62);
+            this.txtLive.Multiline = true;
+            this.txtLive.Name = "txtLive";
+            this.txtLive.Size = new System.Drawing.Size(239, 155);
+            this.txtLive.TabIndex = 2;
+            // 
+            // txtDie
+            // 
+            this.txtDie.Location = new System.Drawing.Point(549, 62);
+            this.txtDie.Multiline = true;
+            this.txtDie.Name = "txtDie";
+            this.txtDie.Size = new System.Drawing.Size(239, 155);
+            this.txtDie.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(93, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Unknown";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(386, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Live";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(658, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Die";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(83, 284);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(287, 20);
+            this.txtFilePath.TabIndex = 7;
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.Location = new System.Drawing.Point(376, 283);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(42, 23);
+            this.btnLoadFile.TabIndex = 8;
+            this.btnLoadFile.Text = "...";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.btnLoadFile_Click);
+            // 
+            // txtRaw
+            // 
+            this.txtRaw.Location = new System.Drawing.Point(549, 262);
+            this.txtRaw.Multiline = true;
+            this.txtRaw.Name = "txtRaw";
+            this.txtRaw.Size = new System.Drawing.Size(239, 155);
+            this.txtRaw.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 288);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Chọn file:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtRaw);
+            this.Controls.Add(this.btnLoadFile);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDie);
+            this.Controls.Add(this.txtLive);
+            this.Controls.Add(this.txtUnknown);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Check live CCV";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtUnknown;
+        private System.Windows.Forms.TextBox txtLive;
+        private System.Windows.Forms.TextBox txtDie;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnLoadFile;
+        private System.Windows.Forms.TextBox txtRaw;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CheckLiveCCV/Form1.cs
@@ -0,0 +1,145 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.IO;
+using System.Threading;
+using System.Web;
+using System.Windows.Forms;
+
+namespace CheckLiveCCV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        public class Root
+        {
+            public int error { get; set; }
+            public string msg { get; set; }
+        }
+
+        async void Request(string dt)
+        {
+            var options = new RestClientOptions()
+            {
+                MaxTimeout = -1,
+                UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+            };
+            var client = new RestClient(options);
+            var request = new RestRequest("https://uncoder.eu.org/cc-checker/api.php", Method.Post);
+            request.AddHeader("accept", "*/*");
+            request.AddHeader("accept-language", "en-US,en;q=0.9,vi;q=0.8,ar;q=0.7,de;q=0.6");
+            request.AddHeader("content-type", "application/x-www-form-urlencoded; charset=UTF-8");
+            request.AddHeader("origin", "https://uncoder.eu.org");
+            request.AddHeader("priority", "u=1, i");
+            request.AddHeader("referer", "https://uncoder.eu.org/cc-checker/");
+            request.AddHeader("sec-ch-ua", "\"Chromium\";v=\"124\", \"Google Chrome\";v=\"124\", \"Not-A.Brand\";v=\"99\"");
+            request.AddHeader("sec-ch-ua-mobile", "?0");
+            request.AddHeader("sec-ch-ua-platform", "\"Windows\"");
+            request.AddHeader("sec-fetch-dest", "empty");
+            request.AddHeader("sec-fetch-mode", "cors");
+            request.AddHeader("sec-fetch-site", "same-origin");
+            request.AddHeader("x-requested-with", "XMLHttpRequest");
+            var body = Encode(dt);
+            request.AddParameter("application/x-www-form-urlencoded", body, ParameterType.RequestBody);
+            RestResponse response = await client.ExecuteAsync(request);
+            Root parsed_json = JsonConvert.DeserializeObject<Root>(response.Content);
+
+            if (parsed_json != null)
+            {
+                string msg = parsed_json.msg;
+
+                if (msg.Contains("Unknown"))
+                {
+                    try
+                    {
+                        txtUnknown.Text += dt + Environment.NewLine;
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Lỗi!");
+                    }
+                }
+                else if (msg.Contains("Live"))
+                {
+                    try
+                    {
+                        txtLive.Text += dt + Environment.NewLine;
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Lỗi!");
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        txtDie.Text += dt + Environment.NewLine;
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Lỗi!");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lỗi kết nối đến server!");
+            }
+
+        }
+
+        public string Encode(string card)
+        {
+            string encode_string = string.Empty;
+            encode_string = HttpUtility.UrlEncode(card);
+            return "data=" + encode_string;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Request(txtRaw.Text);
+            var linesRead = File.ReadLines(txtFilePath.Text);
+            foreach (var lineRead in linesRead)
+            {
+                Thread.Sleep(100);
+                Request(lineRead);
+            }
+        }
+        private void btnLoadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    txtRaw.Text = File.ReadAllText(openFileDialog1.FileName);
+                    txtFilePath.Text = openFileDialog1.FileName;
+                    btnStart.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //btnStart.Enabled = false;
+        }
+    }
+}
+
+
